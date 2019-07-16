90793898f5e6502180e903319b09b53702a8dd06
Jason Ody <jasonody@users.noreply.github.com>
2019-07-15 21:06:48 -0700

    methods (implemented on struct instances)

--- the-rust-programming-language/rectangles/src/main.rs
+++ the-rust-programming-language/rectangles/src/main.rs
@@ -4,6 +4,19 @@ struct Rectangle {
     height: u32,
 }
 
+//implementation block
+impl Rectangle {
+    //area method defined on Rectangle instance
+    fn area(&self) -> u32 { //reading (&self), mutating (&mut self), or consuming (self)*
+        //* https://doc.rust-lang.org/book/ch05-03-method-syntax.html
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool { //other: immutable borrow
+        self.height > other.height && self.width > other.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -15,8 +28,20 @@ fn main() {
         area(&rect1)
     );
 
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
     println!("rect1 is {:?}", rect1);
     println!("rect1 is {:#?}", rect1);
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 20
+    };
+
+    println!("can rect1 hold rect2? {}", rect1.can_hold(&rect2));
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
